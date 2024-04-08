2841955c7a60fcd53b3a5487be27c4a3edfa1b0f
Wongani <wonganichulu@gmail.com>
2024-04-08 17:24:08 +0200

    Added 1-rectangle.py fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,13 +6,15 @@ class Rectangle:
     """Represent a rectangle"""
     def __init__(self, width=0, height=0):
         """Initialises width and height when instance is created"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
+    @property
     def width(self):
         """Returns width of instance"""
         return self.__width
 
+    @width.setter
     def width(self, value):
         """Method that sets width"""
         if not isinstance(value, int):
@@ -21,10 +23,12 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
     def height(self):
         """Returns height of instance"""
         return self.__height
 
+    @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
