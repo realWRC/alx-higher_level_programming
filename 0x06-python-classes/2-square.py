4e9d80b8026a7a87f1bf50be8bdd1ef3798d9dcd
Wongani <wonganichulu@gmail.com>
2024-04-08 15:10:57 +0200

    Added 2-square.py fix

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,8 +5,9 @@
 class Square():
     """Class represents a square."""
     def __init__(self, size=0):
+        """Initilising data and exception checking."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.size = size
+        self.__size = size
