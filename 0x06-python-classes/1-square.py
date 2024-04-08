12ab75c9003e1aeed130b0b2d7aca18cf4d99d81
Wongani <wonganichulu@gmail.com>
2024-04-08 14:39:06 +0200

    Added 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines an empty class called Square"""
+
+
+class Square:
+    """Class represents a square with size attribute."""
+    def ___init___(self, size):
+        self.size = size
+    pass
