2f80b1bd0d1dc6fe5cff1e0eedfaa77955d6e68c
Wongani <wonganichulu@gmail.com>
2024-04-08 15:46:56 +0200

    Added 4-square.py fix

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,11 +6,11 @@ class Square():
     """Class represents a square."""
     def __init__(self, size=0):
         """Initilising data and exception checking."""
-        self.__size = size
+        self.size = size
 
     def size(self):
         """Method that returns current size"""
-        return self.__size
+        return self.size
     
     def size(self, value):
         """Method that resets size"""
@@ -18,8 +18,8 @@ class Square():
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        self.size = value
 
     def area(self):
         """Method that returns area"""
-        return self.__size * self.__size
+        return self.size * self.size
