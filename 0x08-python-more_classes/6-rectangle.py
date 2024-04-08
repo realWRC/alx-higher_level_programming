f704d23f09c37c137d18be86eb054bb3a4e35679
Wongani <wonganichulu@gmail.com>
2024-04-08 21:48:02 +0200

    Added 6-rectangle.py

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""Defines a Class Rectangle"""
+
+
+class Rectangle:
+    """Represent a rectangle"""
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """Initialises width and height when instance is created"""
+        self.width = width
+        self.height = height
+        type(self).number_of_instances += 1
+
+    @property
+    def width(self):
+        """Returns width of instance"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Method that sets width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Returns height of instance"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Method that sets height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Method that returns area of instance"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """Method that returns perimeter of a instance"""
+        if self.__width and self.__height:
+            return (2 * self.__height) + (2 * self.__width)
+
+    def __str__(self):
+        """Return a printable form of a rectangle"""
+        if self.__width and self.__height:
+            shape = []
+            for i in range(self.__height):
+                for j in range(self.__width):
+                    shape.append("#")
+                if i != self.__height - 1:
+                    shape.append("\n")
+            return ("".join(shape))
+
+    def __repr__(self):
+        """Return the string representation of instance"""
+        shape = "Rectangle(" + str(self.__width)
+        shape += ", " + str(self.__height) + ")"
+        return shape
+
+    def __del__(self):
+        """Prints a message for every deletion of instance"""
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
