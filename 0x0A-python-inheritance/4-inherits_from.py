bf8a373136a5a534a845839d24966b9517900c21
Wongani <wonganichulu@gmail.com>
2024-04-15 20:43:49 +0200

    Added 4-inherits_from.py fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,6 +8,7 @@ Contains function for checking for subclasses.
 def inherits_from(obj, a_class):
     """
     Function that checks if obj is a subclass of a_class
+    compares obj and a_class.
     """
     if isinstance(obj, a_class) and type(obj) != a_class:
         return True
