3b8e010b5831175a228c381cd845e2ee8d702444
Wongani <wonganichulu@gmail.com>
2024-04-15 21:43:58 +0200

    Added 7-base_geometry.py and tests/7-base_geometry.txt fix

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,16 +8,19 @@ Contains the definition of a Class
 class BaseGeometry:
     """
     Empty geometry class
+    Methods to be added
     """
     def area(self):
         """
         Method that prints a message
+        Strictky as a place holder
         """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """
         Method that validates value
+        Using type function
         """
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
