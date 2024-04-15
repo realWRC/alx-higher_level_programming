52117203a23bb5fde410dfcb477a76ab68e72659
Wongani <wonganichulu@gmail.com>
2024-04-15 22:15:27 +0200

    Added 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module: 10-square.py
+Contains definition of class Square
+"""
+
+
+Rectangle = __init__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Defines a subclass of Rectangle Square"""
+
+    def __init__(self, size):
+        """Instantiates object of class Square"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Returns the area of a Square"""
+        return size ** 2
