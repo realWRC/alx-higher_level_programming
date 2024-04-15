4afd42a71370575d4491b1c3d3531a7e0a737e97
Wongani <wonganichulu@gmail.com>
2024-04-15 20:39:31 +0200

    Added 4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module: 4-inherits_from.py
+Contains function for checking for subclasses.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Function that checks if obj is a subclass of a_class
+    """
+    if isinstance(obj, a_class) and type(obj) != a_class:
+        return True
+    else:
+        return False
