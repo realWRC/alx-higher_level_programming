dacbe493b86394926969b1bc89b7507bbea8c368
Wongani <wonganichulu@gmail.com>
2024-04-15 21:56:40 +0200

    Added 8-rectangle.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module: 8-rectangle.py
+Defines child class Rectangle.
+"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Definition of child class rectangel."""
+    def __init__(self, width, height):
+        """Intantiates width and height"""
+        self.integer_validator(width)
+        self.integer_validator(height)
+        self.__width = width
+        self.__height = height
