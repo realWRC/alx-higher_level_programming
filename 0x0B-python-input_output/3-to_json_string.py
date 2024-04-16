daa4c0aa6130babe0a16fb557194d7b813e55675
Wongani <wonganichulu@gmail.com>
2024-04-16 15:29:01 +0200

    Added 3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+"""
+Module: 3-to_json_string.py
+Defines functions for handling JSON files.
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Function that returns the JSON string representation
+    of a Python object.
+    """
+    return json.dumps(my_obj)
