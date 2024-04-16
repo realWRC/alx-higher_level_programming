220d24044d8f13850fa28bdbd1886eaa9ec52c86
Wongani <wonganichulu@gmail.com>
2024-04-16 18:03:31 +0200

    Added 8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Function that returns the directory description
+with simple data structure.
+"""
+
+
+def class_to_json(obj):
+    """
+    Function that returns the directory description
+    """
+    return obj.__dict__
