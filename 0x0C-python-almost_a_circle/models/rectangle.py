d705cff7582ad4db6330615bd59f7532675d2dd6
Wongani <wonganichulu@gmail.com>
2024-05-15 11:06:43 +0200

    Added Rectangles fixes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,6 +11,7 @@ class Rectangle(Base):
     subclass of class Base
     """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """ Initialises and validates class info """
         self.validate("width", width)
         self.validate("height", height)
         self.validate("x", x)
@@ -23,42 +24,51 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """ Getter  """
         return self.__width
 
     @property
     def height(self):
+        """ Getter  """
         return self.__height
 
     @property
     def x(self):
+        """ Getter  """
         return self.__x
 
     @property
     def y(self):
+        """ Getter  """
         return self.__y
 
     @width.setter
     def width(self, value):
+        """ Setter """
         self.validate("width", value)
         self.__width = value
 
     @height.setter
     def height(self, value):
+        """ Setter """
         self.validate("height", value)
         self.__height = value
 
     @x.setter
     def x(self, value):
+        """ Setter  """
         self.validate("x", value)
         self.__x = value
 
     @y.setter
     def y(self, value):
+        """ Setter  """
         self.validate("y", value)
         self.__y = value
 
     @staticmethod
     def validate(attribute, value):
+        """ Validates attributes """
         if type(value) != int:
             raise TypeError("{} must be an integer".format(attribute))
         if (attribute == 'x' or attribute == 'y') and (value < 0):
