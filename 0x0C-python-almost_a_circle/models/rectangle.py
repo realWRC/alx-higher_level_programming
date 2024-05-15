dd99adb9e99e1f7c17abefabfdf67528cd47839f
Wongani <wonganichulu@gmail.com>
2024-05-15 11:52:11 +0200

    Added __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,3 +84,10 @@ class Rectangle(Base):
             for j in range(0, self.__width):
                 print("#", end="")
             print()
+
+    def __str__(self):
+        """Returns string representation of class"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.__class__, self.id,
+                                                       self.__x, self.__y,
+                                                       self.__width,
+                                                       self.__height)
