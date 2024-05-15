1cbba71a02ec88ae934fa741f62fc52f70cbae64
Wongani <wonganichulu@gmail.com>
2024-05-15 13:57:38 +0200

    Added Display #1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -80,7 +80,11 @@ class Rectangle(Base):
 
     def display(self):
         """Print rectangle"""
+        for y in range(0, self.__y):
+            print()
         for i in range(0, self.__height):
+            for x in range(0, self.__x):
+                print(" ", end="")
             for j in range(0, self.__width):
                 print("#", end="")
             print()
