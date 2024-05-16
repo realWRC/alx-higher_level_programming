78ab0d95df4e2216a0052ee691956bfe1a4092f8
Wongani <wonganichulu@gmail.com>
2024-05-16 08:36:48 +0200

    Added to_json_string function

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,14 +3,22 @@
 Module base: Defines class Base
 """
 
+import json
+
 
 class Base:
     """The definition of clas base"""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initialises object of class Base"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ Returns a json string of class Base Object
+        """
+        return json.dumps(list_dictionaries)
