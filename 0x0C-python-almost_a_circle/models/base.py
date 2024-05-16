1d3b3e8acd6df836fad744c6744ee6149c6257ab
Wongani <wonganichulu@gmail.com>
2024-05-16 10:33:29 +0200

    Added save to json file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,7 +17,7 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-    
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """ Returns a json string of class Base Object
@@ -29,3 +29,14 @@ class Base:
         if not all(type(element) == dict for element in list_dictionaries):
             raise TypeError("list_dictionaries must be a list of dictionries")
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Class method that writes to a JSON file """
+        if list_objs is None or list_objs == []:
+            data = "[]"
+        else:
+            data = cls.to_json_string([x.to_dictionary() for x in list_objs])
+        name = cls.__name__ + ".json"
+        with open(name, 'w') as j_file:
+            j_file.write(data)
