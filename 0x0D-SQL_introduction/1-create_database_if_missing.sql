fa6125cdee80e0c58277782da8b766ea0b6a16b1
Wongani <wonganichulu@gmail.com>
2024-05-21 20:45:08 +0200

    Added a README.md, 0-list_database.sql and 1-create_database_if_missing.sql

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- creates the database hbtn_0c_0 in your MySQL server
+-- If the database hbtn_0c_0 already exists the script will fail
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/README.md
@@ -0,0 +1,1 @@
+This is the directory for the 0x0D. SQL - Introduction project.
