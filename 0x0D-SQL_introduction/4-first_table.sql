4c00498e7acea44b70fb747bb24e8ff96a1cc3c9
Wongani <wonganichulu@gmail.com>
2024-05-21 20:58:44 +0200

    Added 4-first_table.sql and 5-full_table.sql

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- A script that creates a table called first_table in the current database in your MySQL server
+-- If the table first_table already exists, the script will not fail
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
