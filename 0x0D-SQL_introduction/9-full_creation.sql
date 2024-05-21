39dbaf2dc626a153e6eb7ce6dced28301b62222f
Wongani <wonganichulu@gmail.com>
2024-05-21 22:01:36 +0200

    Added fix for 9-full_creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,6 @@
 -- A script that creates a table second_table in the database hbtn_0c_0
 -- in MySQL server and adds multiples rows.
-CREATE TABLE IF NOT EXISTS (id INT, name VARCHAR(256), score INT);
--- Inserts data into the table
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score)
 VALUES (1, 'John', 10),
 (2, 'Alex', 3),
