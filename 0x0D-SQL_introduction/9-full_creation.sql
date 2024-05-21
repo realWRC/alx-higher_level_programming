75479ce370e2d7ad57cb49405255b01f07a0eb68
Wongani <wonganichulu@gmail.com>
2024-05-21 21:38:29 +0200

    9-full_creation.sql fix

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,7 +2,8 @@
 -- in MySQL server and adds multiples rows.
 CREATE TABLE IF NOT EXISTS (id INT, name VARCHAR(256), score INT);
 -- Inserts data into the table
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, 'John', 10);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, 'Alex', 3);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, 'Bob', 14);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, 'George', 8);
+INSERT INTO second_table (id, name, score)
+VALUES (1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
