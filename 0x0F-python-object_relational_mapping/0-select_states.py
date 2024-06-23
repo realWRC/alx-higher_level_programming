61b18ff2ba105e260e6dbff89338b871eafe9c14
Wongani <wonganichulu@gmail.com>
2024-06-23 13:03:25 +0200

    Added 0-select_states.py fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,21 +7,13 @@ script that lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 from sys import argv
 
-try:
-    db_connector = MySQLdb.connect(
-        host='localhost', user=argv[1], port=3306, passwd=argv[2], db=argv[3]
-    )
+db_connector = MySQLdb.connect(
+    host='localhost', user=argv[1], port=3306, passwd=argv[2], db=argv[3]
+)
 
-    cur = db_connector.cursor()
-    cur.execute(f'SELECT * FROM states ORDER BY id')
-    rows = cur.fetchall()
+cur = db_connector.cursor()
+cur.execute(f'SELECT * FROM states ORDER BY id')
+rows = cur.fetchall()
 
-    for row in rows:
-        print(row)
-
-except MySQLdb.Error as e:
-    print("MySQL Error: ", e)
-finally:
-    if db_connector:
-        cur.close()
-        db_connector.close()
+for row in rows:
+    print(row)
