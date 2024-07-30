00ef4dfddefcfa0dd5fc3e55d2faf90364941318
Wongani <wonganichulu@gmail.com>
2024-07-30 20:47:10 +0200

    Fix added for 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,10 +2,10 @@
 const r = require('request');
 const fs = require('fs');
 
-r(process.argv[2], function (err, response, body){
+r(process.argv[2], function (_err, _response, body) {
   fs.writeFile(process.argv[3], body, 'utf8', function (err) {
     if (err) {
       console.log(err);
     }
-  })
-})
+  });
+});
