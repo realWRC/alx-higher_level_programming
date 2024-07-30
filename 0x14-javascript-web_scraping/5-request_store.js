2dd88be1254d97aadd996fadffe0876f4412645f
Wongani <wonganichulu@gmail.com>
2024-07-30 20:42:58 +0200

    Added 5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const r = require('request');
+const fs = require('fs');
+
+r(process.argv[2], function (err, response, body){
+  fs.writeFile(process.argv[3], body, 'utf8', function (err) {
+    if (err) {
+      console.log(err);
+    }
+  })
+})
