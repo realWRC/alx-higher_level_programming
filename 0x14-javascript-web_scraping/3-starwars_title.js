4f8fd05269a5f64311df792c8b80c7d7334bfe6b
Wongani <wonganichulu@gmail.com>
2024-07-30 20:03:26 +0200

    Added 3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const r = require('request');
+const star_wars = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${star_wars}`;
+
+r.get(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
+});
