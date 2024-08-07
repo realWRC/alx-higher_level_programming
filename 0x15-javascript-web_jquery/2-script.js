ee82c128b443d478cdc697bff596b2bd377415b4
Wongani <wonganichulu@gmail.com>
2024-08-07 21:37:08 +0200

    2-script.js: Added

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,6 @@
+// updates the text color of the <header> element to red (#FF0000)
+$(document).ready(function() {
+  $('#red_header').click(function() {
+    $('header').css('color', '#FF0000');
+  });
+});
