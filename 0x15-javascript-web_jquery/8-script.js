209ba27032ac66701a839f4634823a1235cf553e
Wongani <wonganichulu@gmail.com>
2024-08-07 22:06:33 +0200

    Added 8-script.js

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $.getJSON("https://swapi-api.alx-tools.com/api/films/?format=json", function(data) {
+    data.results.forEach(function(movie) {
+      $("<li>").text(movie.title).appendTo("#list_movies");
+    });
+  });
+});
